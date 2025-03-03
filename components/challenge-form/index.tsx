c717145d1aad647d9a034973a698df40685297c6
antoniogdelrio <antoniogdelrio@hotmail.com>
2025-03-03 17:01:29 -0300

    feat: create static challenge form page

---
+++ components/challenge-form/index.tsx
@@ -0,0 +1,52 @@
+'use client'
+
+import { Select, SelectContent, SelectItem, SelectTrigger, SelectValue } from "@/components/ui/select"
+import { Label } from "../ui/label"
+import { Checkbox } from "../ui/checkbox"
+
+const ChallengeForm = () => {
+  return (
+    <form>
+      <div className="mb-4">
+        <Label htmlFor="challengeSize" className="mb-2">
+          <p className="text-base font-medium">Quantidade de Questões</p>
+        </Label>
+        <Select>
+          <SelectTrigger className="w-[100%]" id="challengeSize">
+            <SelectValue placeholder="Quantidade de Questões" />
+          </SelectTrigger>
+          <SelectContent>
+            <SelectItem value="5">5</SelectItem>
+            <SelectItem value="10">10</SelectItem>
+            <SelectItem value="15">15</SelectItem>
+          </SelectContent>
+        </Select>
+      </div>
+      <fieldset>
+        <legend className="mb-2">
+          <p className="text-base font-medium">Cadernos</p>
+        </legend>
+        <div className="flex flex-wrap">
+          <div className="flex gap-1 items-center w-[100%] mb-4 sm:w-[50%]">
+            <Checkbox id="matematica" />
+            <Label htmlFor="matematica">Matemática</Label>
+          </div>
+          <div className="flex gap-1 items-center w-[100%] mb-4 sm:w-[50%]">
+            <Checkbox id="natureza" />
+            <Label htmlFor="natureza">Ciências da Natureza</Label>
+          </div>
+          <div className="flex gap-1 items-center w-[100%] mb-4 sm:w-[50%]">
+            <Checkbox id="linguagens" />
+            <Label htmlFor="linguagens">Linguagens e Códigos</Label>
+          </div>
+          <div className="flex gap-1 items-center w-[100%] mb-4 sm:w-[50%]">
+            <Checkbox id="humanas" />
+            <Label htmlFor="humanas">Ciências Humanas</Label>
+          </div>
+        </div>
+      </fieldset>
+    </form>
+  )
+}
+
+export default ChallengeForm

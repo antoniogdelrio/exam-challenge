9f26b6178f2fee8f222dd43edacd588e2dda5309
antoniogdelrio <antoniogdelrio@hotmail.com>
2025-04-29 22:44:11 -0300

    refactor: remove wrapper div from link page

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -11,21 +11,19 @@ export default async function Link(props: {
 
   const challengeLink = await verifyCode(params.code)
   return (
-    <div>
-      <main className="flex flex-col items-center mx-3">
-        <h2 className="text-3xl font-bold mb-8">Desafio gerado!</h2>
-        <p className="mb-6">Envie o link para um amigo</p>
-        <div className="flex flex-col gap-1 sm:flex-row w-full">
-          <InputCopy
-            ToastIcon={<Check />}
-            toastMessage="Link copiado para a área de transferência"
-            inputProps={{
-              placeholder: "Copiar link",
-              value: challengeLink
-            }}
-          />
-        </div>
-      </main>
-    </div>
+    <main className="flex flex-col items-center mx-3">
+      <h2 className="text-3xl font-bold mb-8">Desafio gerado!</h2>
+      <p className="mb-6">Envie o link para um amigo</p>
+      <div className="flex flex-col gap-1 sm:flex-row w-full">
+        <InputCopy
+          ToastIcon={<Check />}
+          toastMessage="Link copiado para a área de transferência"
+          inputProps={{
+            placeholder: "Copiar link",
+            value: challengeLink
+          }}
+        />
+      </div>
+    </main>
   );
 }

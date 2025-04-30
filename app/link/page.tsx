7ed4ccac17e16a399bffb063f63767b8bd281c9f
antoniogdelrio <antoniogdelrio@hotmail.com>
2025-04-29 23:06:32 -0300

    feat: add link to go direct to challenge

--- app/link/page.tsx
+++ app/link/page.tsx
@@ -1,10 +1,12 @@
+import Link from "next/link";
 import InputCopy from "@/components/ui/input-copy";
 import { verifyCode } from "@/lib/actions";
-import { Check } from "lucide-react";
+import { Check, Goal } from "lucide-react";
+import { Button } from "@/components/ui/button";
 
 type SearchParams = Promise<{ [key: string]: string }>
 
-export default async function Link(props: {
+export default async function LinkPage(props: {
   searchParams: SearchParams
 }) {
   const params = await props.searchParams
@@ -13,7 +15,10 @@ export default async function Link(props: {
   return (
     <main className="flex flex-col items-center mx-3">
       <h2 className="text-3xl font-bold mb-8">Desafio gerado!</h2>
-      <p className="mb-6">Envie o link para um amigo</p>
+      <Button asChild className="w-full mb-8">
+        <Link href={challengeLink}>Ir para o desafio <Goal /></Link>
+      </Button>
+      <p className="mb-6">ou envie o link para um amigo</p>
       <div className="flex flex-col gap-1 sm:flex-row w-full">
         <InputCopy
           ToastIcon={<Check />}
